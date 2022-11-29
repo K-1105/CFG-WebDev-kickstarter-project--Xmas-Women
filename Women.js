37b51d382672e1760a56dccdb45d2cfc750abf63
MollyRushton <mollyrushton@gmail.com>
2022-11-29 20:52:03 +0000

    Upload

---
+++ Women.js
@@ -0,0 +1,47 @@
+let person1 = {
+    first_name: "Emmeline",
+    last_name: "Pankhurst",
+    famous_for: "leading the suffragette movement that would lead to women being granted voting rights in 1918",
+    dob: "1858-07-14",
+    dod: "1928-06-14",
+    age_at_death: 69,
+    famous_decade: "1920s",
+    zodiac_sign: "Cancer",
+    birth_place: "Manchester",
+    profession: "politician",
+    height:null,
+    maiden_name: "Goulden",
+    aka: "Emily Goulden",
+    books: "My Own Story",
+    quotes: (
+        "We are here, not because we are law-breakers; we are here in our efforts to become law-makers.",
+        "I am what you call a hooligan",
+        "You have to make more noise than anybody else"
+        ),
+    number_of_siblings: 9,
+    school: "École Normale de Neuilly"
+}
+
+let person2 = {
+    first_name: "Hattie",
+    last_name: "McDaniel",
+    famous_for: "the first African-American actor to break the colour barrier and receive an Oscar in 1939 for playing 'Mammy' in 'Gone with the Wind' ",
+    dob: "1893-06-10",
+    dod: "1952-10-26",
+    age_at_death: 59,
+    famous_decade: "1930s",
+    zodiac_sign: "Gemini",
+    birth_place: "Wichita, Kansas",
+    profession: "Actress, singer-songwriter and comedian",
+    height:null,
+    maiden_name: null,
+    aka: null,
+    books: null,
+    quotes: (
+        "I've learned by livin' and watchin' that there is only eighteen inches between a pat on the back and a kick in the seat of the pants..",
+        "The entire race is usually judged by the actions of one man or woman",
+        "I am proud of my standing as a woman interested in her race"
+        ),
+    number_of_siblings: 12,
+    school: "Denver East High School"
+}
